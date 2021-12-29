63e72a2645bab9320983a47a457ca3942d35eec9
joao <joaof082@gmail.com>
2021-12-29 00:44:04 -0300

    refact: Desabilitando a statusBar
    Signed-off-by: joao <joaof082@gmail.com>

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ import {Home} from './src/Pages/Home';
 export default function App() {
   return (
     <>
-      <StatusBar barStyle="dark-content" />
+      <StatusBar hidden={true} />
       <Home />
     </>
   );
